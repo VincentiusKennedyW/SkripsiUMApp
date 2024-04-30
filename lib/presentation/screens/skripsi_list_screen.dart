7244bd85ec9e8c874e4960ecc0834bc807e3d0e4
VincentiusKennedyW <kenken.kk40@gmail.com>
2024-05-01 01:00:48 +0800

    update skripsi list screen

--- lib/presentation/screens/skripsi_list_screen.dart
+++ lib/presentation/screens/skripsi_list_screen.dart
@@ -2,15 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:skripsi_mulia_app/presentation/bloc/login_bloc/login_bloc.dart';
+import 'package:skripsi_mulia_app/presentation/bloc/skripsi_bloc/skripsi_bloc.dart';
 
-class SkripsiListScreen extends StatelessWidget {
+class SkripsiListScreen extends StatefulWidget {
   const SkripsiListScreen({super.key});
+
+  @override
+  State<SkripsiListScreen> createState() => _SkripsiListScreenState();
+}
+
+class _SkripsiListScreenState extends State<SkripsiListScreen> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    context.read<SkripsiBloc>().add(const SkripsiEvent.getSkripsi());
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >=
+          scrollController.position.maxScrollExtent) {
+        context.read<SkripsiBloc>().add(const SkripsiEvent.getNextSkripsi());
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Restaurant'),
+        title: const Text('Skripsi Universitas Mulia'),
         actions: [
           IconButton(
               icon: const Icon(Icons.logout),
@@ -44,7 +70,55 @@ class SkripsiListScreen extends StatelessWidget {
               })
         ],
       ),
-      body: const Placeholder(),
+      body: BlocBuilder<SkripsiBloc, SkripsiState>(
+        builder: (context, state) {
+          return state.when(
+            skripsiInitial: () => const SizedBox(),
+            skripsiLoading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            skripsiLoaded: (listSkripsi, hasReachedMax) {
+              return ListView.builder(
+                controller: scrollController,
+                shrinkWrap: true,
+                itemCount: (hasReachedMax!)
+                    ? listSkripsi.length
+                    : listSkripsi.length + 1,
+                itemBuilder: (context, index) {
+                  return (index >= listSkripsi.length)
+                      ? const Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(8),
+                            child: CircularProgressIndicator(),
+                          ),
+                        )
+                      : ListTile(
+                          leading: IconButton(
+                            icon: const Icon(Icons.bookmark_border),
+                            onPressed: () {},
+                          ),
+                          title: Text(listSkripsi[index].judul),
+                          subtitle: Text(listSkripsi[index].angkatan),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.arrow_forward_ios),
+                            onPressed: () {
+                              context
+                                  .goNamed('/skripsi/${listSkripsi[index].id}');
+                            },
+                          )
+                          // onTap: () {
+                          //   context.goNamed('/skripsi/${skripsiData.id}');
+                          // },
+                          );
+                },
+              );
+            },
+            skripsiError: (message) => Center(
+              child: Text(message),
+            ),
+          );
+        },
+      ),
     );
   }
 }
