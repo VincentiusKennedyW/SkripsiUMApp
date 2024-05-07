7632b4de0e7233ff7ed7c3e5fd2a50324b577f7f
VincentiusKennedyW <kenken.kk40@gmail.com>
2024-05-07 18:57:25 +0800

    create new widget to show skripsi detail

---
+++ lib/presentation/widget/skripsi_bottomsheet.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skripsi_mulia_app/models/skripsi_models/skripsi_response.dart';
+import 'package:skripsi_mulia_app/presentation/bloc/skripsi_detail_bloc/skripsi_detail_bloc.dart';
+import 'package:skripsi_mulia_app/utils/theme.dart';
+
+class SkripsiBottomSheet extends StatefulWidget {
+  final Skripsi skripsi;
+
+  const SkripsiBottomSheet({
+    super.key,
+    required this.skripsi,
+  });
+
+  @override
+  State<SkripsiBottomSheet> createState() => _SkripsiBottomSheetState();
+}
+
+class _SkripsiBottomSheetState extends State<SkripsiBottomSheet> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<SkripsiDetailBloc>().add(
+          SkripsiDetailEvent.getSkripsiDetail(widget.skripsi.id),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.3,
+      minChildSize: 0.25,
+      expand: false,
+      builder: (context, scrollController) {
+        return BlocBuilder<SkripsiDetailBloc, SkripsiDetailState>(
+          builder: (context, state) {
+            return state.when(
+              skripsiDetailInitial: () => const SizedBox(),
+              skripsiDetailLoading: () => Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xFF395B64),
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+                ),
+                child: const Center(
+                  child: CircularProgressIndicator(
+                    color: fourthColor,
+                  ),
+                ),
+              ),
+              skripsiDetailLoaded: (skripsiDetail) {
+                return Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF395B64),
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(16)),
+                  ),
+                  padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 16),
+                        child: SingleChildScrollView(
+                          controller: scrollController,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    skripsiDetail.judul,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(Icons.bookmark_border),
+                                        color: Colors.white,
+                                        onPressed: () {},
+                                      ),
+                                      Text('Simpan Judul Ini',
+                                          style: TextStyle(
+                                            color: Colors.white70,
+                                            fontSize: 14,
+                                          )),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.person_2_outlined,
+                                        color: Colors.red,
+                                        size: 16,
+                                      ),
+                                      const SizedBox(width: 4),
+                                      Text(
+                                        skripsiDetail.nama,
+                                        style: const TextStyle(
+                                          color: Colors.white70,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                skripsiDetail.nim,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                skripsiDetail.angkatan,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          color: Colors.white,
+                          height: 4,
+                          width: 62,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              skripsiDetailError: (message) => Center(
+                child: Text(message),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentation/widget/skripsi_list.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skripsi_mulia_app/models/skripsi_models/skripsi_response.dart';
+import 'package:skripsi_mulia_app/presentation/bloc/skripsi_bloc/skripsi_bloc.dart';
+
+class SkripsiList extends StatefulWidget {
+  final List<Skripsi> listSkripsi;
+  final bool? hasReachedMax;
+  final Function(Skripsi) onSkripsiTap;
+
+  const SkripsiList({
+    super.key,
+    required this.listSkripsi,
+    required this.hasReachedMax,
+    required this.onSkripsiTap,
+  });
+
+  @override
+  _SkripsiListState createState() => _SkripsiListState();
+}
+
+class _SkripsiListState extends State<SkripsiList> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels >=
+          scrollController.position.maxScrollExtent) {
+        context.read<SkripsiBloc>().add(const SkripsiEvent.getNextSkripsi());
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: scrollController,
+      shrinkWrap: true,
+      itemCount: (widget.hasReachedMax!)
+          ? widget.listSkripsi.length
+          : widget.listSkripsi.length + 1,
+      itemBuilder: (context, index) {
+        return (index >= widget.listSkripsi.length)
+            ? const Center(
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: CircularProgressIndicator(),
+                ),
+              )
+            : ListTile(
+                title: Text(widget.listSkripsi[index].judul),
+                subtitle: Text(widget.listSkripsi[index].angkatan),
+                trailing: IconButton(
+                    icon: const Icon(Icons.arrow_forward_ios),
+                    onPressed: () {
+                      widget.onSkripsiTap(widget.listSkripsi[index]);
+                    }),
+              );
+      },
+    );
+  }
+}
