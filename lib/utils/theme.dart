28e21a465f87979fffd83679e8be48ae37211a84
VincentiusKennedyW <kenken.kk40@gmail.com>
2024-05-07 18:54:01 +0800

    change font

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -7,28 +7,28 @@ const Color thirdColor = Color(0xFF11999E);
 const Color fourthColor = Color(0xFFE7F6F2);
 
 final TextTheme myTextTheme = TextTheme(
-  displayLarge: GoogleFonts.prompt(
+  displayLarge: GoogleFonts.poppins(
       fontSize: 92, fontWeight: FontWeight.w300, letterSpacing: -1.5),
-  displayMedium: GoogleFonts.prompt(
+  displayMedium: GoogleFonts.poppins(
       fontSize: 57, fontWeight: FontWeight.w300, letterSpacing: -0.5),
-  displaySmall: GoogleFonts.prompt(fontSize: 46, fontWeight: FontWeight.w400),
-  headlineMedium: GoogleFonts.prompt(
+  displaySmall: GoogleFonts.poppins(fontSize: 46, fontWeight: FontWeight.w400),
+  headlineMedium: GoogleFonts.poppins(
       fontSize: 32, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-  headlineSmall: GoogleFonts.prompt(fontSize: 23, fontWeight: FontWeight.w400),
-  titleLarge: GoogleFonts.prompt(
+  headlineSmall: GoogleFonts.poppins(fontSize: 23, fontWeight: FontWeight.w400),
+  titleLarge: GoogleFonts.poppins(
       fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15),
-  titleMedium: GoogleFonts.prompt(
+  titleMedium: GoogleFonts.poppins(
       fontSize: 15, fontWeight: FontWeight.w400, letterSpacing: 0.15),
-  titleSmall: GoogleFonts.prompt(
+  titleSmall: GoogleFonts.poppins(
       fontSize: 13, fontWeight: FontWeight.w500, letterSpacing: 0.1),
-  bodyLarge: GoogleFonts.prompt(
+  bodyLarge: GoogleFonts.poppins(
       fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
-  bodyMedium: GoogleFonts.prompt(
+  bodyMedium: GoogleFonts.poppins(
       fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
-  bodySmall: GoogleFonts.prompt(
+  bodySmall: GoogleFonts.poppins(
       fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
-  labelLarge: GoogleFonts.prompt(
+  labelLarge: GoogleFonts.poppins(
       fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
-  labelSmall: GoogleFonts.prompt(
+  labelSmall: GoogleFonts.poppins(
       fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
 );
