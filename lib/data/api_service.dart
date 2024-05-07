5cf568137a7ff37e5259b6b1a36db34904753c09
VincentiusKennedyW <kenken.kk40@gmail.com>
2024-05-07 18:55:00 +0800

    add getSkripsiDetail method and change base_url

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:skripsi_mulia_app/models/skripsi_detail_models/skripsi_detail_response.dart';
 import 'package:skripsi_mulia_app/models/skripsi_models/skripsi_response.dart';
 
 class ApiService {
-  static String baseUrl = 'http://192.168.1.9:8001/api';
+  static String baseUrl = 'http://192.168.1.8:8001/api';
 
   Future<SkripsiResponse> getSkripsi(String token, [int page = 1]) async {
     try {
@@ -35,6 +36,36 @@ class ApiService {
       }
     } catch (e) {
       throw Exception("Failed to load skripsi: $e");
+    }
+  }
+
+  Future<SkripsiDetailResponse> getSkripsiDetail(String token, int id) async {
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/skripsi/$id'),
+        headers: <String, String>{
+          'Accept': 'application/json',
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        final skripsiDetail = SkripsiDetail.fromJson(responseData['data']);
+
+        return SkripsiDetailResponse(
+          error: responseData['error'],
+          message: responseData['message'],
+          skripsiDetail: skripsiDetail,
+        );
+      } else {
+        final errorBody = json.decode(response.body);
+        final errorMessage = errorBody['message'];
+        throw Exception(errorMessage ?? "Failed to load skripsi detail");
+      }
+    } catch (e) {
+      throw Exception("Failed to load skripsi detail: $e");
     }
   }
 }
