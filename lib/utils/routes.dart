74736b1b71d65569e3d3e6a40643714edd26a0c5
VincentiusKennedyW <kenken.kk40@gmail.com>
2024-05-21 21:50:08 +0800

    add new routes for gemini

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:skripsi_mulia_app/presentation/screens/auth_screen/login_screen.dart';
 import 'package:skripsi_mulia_app/presentation/screens/auth_screen/register_screen.dart';
 import 'package:skripsi_mulia_app/presentation/screens/bookmark_screen.dart';
+import 'package:skripsi_mulia_app/presentation/screens/gemini_prompt_screen.dart';
 import 'package:skripsi_mulia_app/presentation/screens/home_screen.dart';
 import 'package:skripsi_mulia_app/presentation/screens/profile_screen.dart';
 import 'package:skripsi_mulia_app/presentation/screens/search_screen.dart';
@@ -11,7 +12,9 @@ import 'package:skripsi_mulia_app/presentation/screens/skripsi_list_screen.dart'
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
 final GlobalKey<NavigatorState> _homeNavigatorKey = GlobalKey<NavigatorState>();
-final GlobalKey<NavigatorState> _bookmarkNavigatorKey =
+final GlobalKey<NavigatorState> _searchNavigatorKey =
+    GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> _geminiPromptNavigatorKey =
     GlobalKey<NavigatorState>();
 final GlobalKey<NavigatorState> _profileNavigatorKey =
     GlobalKey<NavigatorState>();
@@ -49,12 +52,22 @@ final router = GoRouter(
           ],
         ),
         StatefulShellBranch(
-          navigatorKey: _bookmarkNavigatorKey,
+          navigatorKey: _searchNavigatorKey,
           routes: [
             GoRoute(
               name: 'search',
               path: '/search',
               builder: (context, state) => const SearchScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          navigatorKey: _geminiPromptNavigatorKey,
+          routes: [
+            GoRoute(
+              name: 'geminiprompt',
+              path: '/geminiprompt',
+              builder: (context, state) => const GeminiPromptScreen(),
             ),
           ],
         ),
